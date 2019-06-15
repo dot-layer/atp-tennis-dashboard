bf3744fd39c17cd6324f8f05f034acff1749d7d7
Stephane Caron <ste.caron@icloud.com>
2019-06-15 14:33:11 -0400

    Set up the shiny app

---
+++ launch-app.R
@@ -0,0 +1,25 @@
+# -------------------------------------------------------------------------
+# Title: launch-app
+# Goal: Launching file for the Shiny App
+# Date: June 15th 2019
+# Author: .Layer
+# -------------------------------------------------------------------------
+
+
+# Load packages -----------------------------------------------------------
+
+library(shiny)
+library(shinydashboard)
+library(data.table)
+library(DT)
+library(ggplot2)
+library(dplyr)
+
+
+# Source functions --------------------------------------------------------
+
+
+
+# Run app -----------------------------------------------------------------
+
+runApp("src/shiny-app/")
